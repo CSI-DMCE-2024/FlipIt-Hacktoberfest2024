601ca8bd30562669e6d99ea3693fb33b100a0838
Vibhor Mathur <99306756+Matvibhor24@users.noreply.github.com>
2024-10-07 14:19:57 +0530

    Added Fade in animation to splash screen

--- app/src/main/java/com/example/memorytilegame/SplashScreen.java
+++ app/src/main/java/com/example/memorytilegame/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.example.memorytilegame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,12 +9,23 @@ import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private MotionLayout motionLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        motionLayout = findViewById(R.id.splashScreenAnim);
+
         Intent intent = new Intent(this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                motionLayout.transitionToEnd();
+            }
+        }, 1000);
 
         new Handler().postDelayed(new Runnable() {
             @Override
